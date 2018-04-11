615ab4b8f6edbb4dd6bb808c99edfa941bc35ac9
fscheugenpflug <fs.scheugenpflug@hotmail.de>
2018-04-11 22:46:41 +0200

    BCN Fabian

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,15 +1,17 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Make everything inside of public/ available
+
 app.use(express.static('public'));
 
 // Pages
 
 app.get('/', (request, response, next) => {
-  response.sendFile(__dirname + '/index.html')
+  response.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('/about', (request, response, next) => {
@@ -21,5 +23,5 @@ app.get('/gallery', (request, response, next) => {
 });
 
 app.listen(3000, () => {
-  console.log('My first app listening on port 3000!')
-}); 
+  console.log('My first app listening on port 3000!');
+});
